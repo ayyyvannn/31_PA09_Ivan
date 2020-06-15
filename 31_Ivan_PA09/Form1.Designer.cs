5523a2417c4dea18874ae9f610161f5341e0b9eb
ayyyvannn <64124308+ayyyvannn@users.noreply.github.com>
2020-06-15 17:03:34 +0800

    Currency converter project by Ivan

---
+++ 31_Ivan_PA09/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace _31_Ivan_PA09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Amount = new System.Windows.Forms.TextBox();
+            this.txt_Converted = new System.Windows.Forms.TextBox();
+            this.btn_Convert = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.rb_US = new System.Windows.Forms.RadioButton();
+            this.rb_Yen = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Currency";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Converted Amount";
+            // 
+            // txt_Amount
+            // 
+            this.txt_Amount.Location = new System.Drawing.Point(234, 134);
+            this.txt_Amount.Name = "txt_Amount";
+            this.txt_Amount.Size = new System.Drawing.Size(174, 20);
+            this.txt_Amount.TabIndex = 3;
+            // 
+            // txt_Converted
+            // 
+            this.txt_Converted.Location = new System.Drawing.Point(234, 253);
+            this.txt_Converted.Name = "txt_Converted";
+            this.txt_Converted.Size = new System.Drawing.Size(174, 20);
+            this.txt_Converted.TabIndex = 4;
+            // 
+            // btn_Convert
+            // 
+            this.btn_Convert.Location = new System.Drawing.Point(462, 196);
+            this.btn_Convert.Name = "btn_Convert";
+            this.btn_Convert.Size = new System.Drawing.Size(94, 32);
+            this.btn_Convert.TabIndex = 5;
+            this.btn_Convert.Text = "Convert";
+            this.btn_Convert.UseVisualStyleBackColor = true;
+            this.btn_Convert.Click += new System.EventHandler(this.btn_Convert_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(462, 246);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(94, 32);
+            this.btn_Clear.TabIndex = 6;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // rb_US
+            // 
+            this.rb_US.AutoSize = true;
+            this.rb_US.Location = new System.Drawing.Point(234, 188);
+            this.rb_US.Name = "rb_US";
+            this.rb_US.Size = new System.Drawing.Size(75, 17);
+            this.rb_US.TabIndex = 7;
+            this.rb_US.TabStop = true;
+            this.rb_US.Text = "US Dollars";
+            this.rb_US.UseVisualStyleBackColor = true;
+            // 
+            // rb_Yen
+            // 
+            this.rb_Yen.AutoSize = true;
+            this.rb_Yen.Location = new System.Drawing.Point(234, 211);
+            this.rb_Yen.Name = "rb_Yen";
+            this.rb_Yen.Size = new System.Drawing.Size(93, 17);
+            this.rb_Yen.TabIndex = 8;
+            this.rb_Yen.TabStop = true;
+            this.rb_Yen.Text = "Japanese Yen";
+            this.rb_Yen.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 493);
+            this.Controls.Add(this.rb_Yen);
+            this.Controls.Add(this.rb_US);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Convert);
+            this.Controls.Add(this.txt_Converted);
+            this.Controls.Add(this.txt_Amount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Amount;
+        private System.Windows.Forms.TextBox txt_Converted;
+        private System.Windows.Forms.Button btn_Convert;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.RadioButton rb_US;
+        private System.Windows.Forms.RadioButton rb_Yen;
+    }
+}
+
